4af5364b182c69ba839c35d6dfe50f99e7a0e85d
Spratt93 <tc3g20@soton.ac.uk>
2023-03-12 10:15:31 +0000

    navbar working

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <div class="navbar">
+        <ul>
+            <li>
+                <RouterLink to="/">Home</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/projects">Projects</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/experience">Experience</RouterLink>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.navbar {
+    padding: 2em;
+    position: fixed;
+    top: 0em;
+}
+
+ul {
+    list-style-type: none;
+    width: 40em;
+}
+
+li {
+    float: left;
+    margin-left: 5em;
+}</style>
